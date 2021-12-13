b40a196b8ee331641c5747f0fe9078f6626547a5
Fazil Ahamed K <seyadian@gmail.com>
2021-12-13 22:24:51 +0530

    Base Project

---
+++ src/main/java/in/awakers/bursar/AwakersBursarApplication.java
@@ -0,0 +1,13 @@
+package in.awakers.bursar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwakersBursarApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AwakersBursarApplication.class);
+    }
+}
